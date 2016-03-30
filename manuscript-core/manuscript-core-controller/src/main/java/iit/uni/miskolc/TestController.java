02fe8fa9d3c37abe68efa160f664d3a40906c830
Balázs Kovács <kovats.balazs25@gmail.com>
2016-03-30 13:56:02 +0200

    basic app.js has added. Angularjs has already added. the mess of dependencies has already cleaned up

--- manuscript-core/manuscript-core-controller/src/main/java/iit/uni/miskolc/TestController.java
+++ manuscript-core/manuscript-core-controller/src/main/java/iit/uni/miskolc/TestController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TestController {
 
-	
-	@RequestMapping ("/index")
+	@RequestMapping("/index")
 	public String Hello(Model model) {
 		return "index";
 	}
