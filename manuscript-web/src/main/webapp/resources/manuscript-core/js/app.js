b4de75022ee050dd6c0cc4ebf82667e9b7a4cc99
Balazs Kovacs <kovats.balazs25@gmail.com>
2016-04-04 23:25:11 +0200

    spring security has updated. There is three roles without different permissions

--- manuscript-web/src/main/webapp/resources/manuscript-core/js/app.js
+++ manuscript-web/src/main/webapp/resources/manuscript-core/js/app.js
@@ -1,1 +1,10 @@
-var app = angular.module("app", []); 
+var app = angular.module("app", []);
+//app.controller('ctrl', function($scope, $http) {
+//	$scope.sendPost = function(){
+//		$http.post('message').success(function(data, status) {
+//            $scope.hello = data;
+//        }).error(function(response){
+//        	console.out(response)
+//        })
+//	}
+//})
