4bf5dd12c64916512f2814981e3415f25c4cd93c
Beni312 <bence312@gmail.com>
2016-04-10 02:57:51 +0200

    Create UserMapper.java

---
+++ manuscript-core/manuscript-core-persist-mysql/src/main/java/mappers/UserMapper.java
@@ -0,0 +1,19 @@
+package iit.uni.miskolc.user.mapper;
+
+import iit.uni.miskolc.user.registration.User;
+
+/**
+ * 
+ * @author Bence Varadi
+ *
+ */
+public interface UserMapper {
+	
+	User selectUserByID(String id);
+	
+	void insertUser(User user);
+	
+	void deleteUserByID(String id);
+	
+	void editUserData(User user);
+}
