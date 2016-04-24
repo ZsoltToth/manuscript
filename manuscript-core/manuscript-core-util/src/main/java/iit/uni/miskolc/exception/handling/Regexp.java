4a07b019c5bb33b2b5ac4c0584cca1b560f9d47f
Beni312 <bence312@gmail.com>
2016-04-24 11:19:07 +0200

    Update Regexp.java

--- manuscript-core/manuscript-core-util/src/main/java/iit/uni/miskolc/exception/handling/Regexp.java
+++ manuscript-core/manuscript-core-util/src/main/java/iit/uni/miskolc/exception/handling/Regexp.java
@@ -7,10 +7,12 @@ package iit.uni.miskolc.exception.handling;
  */
 public class Regexp {
 	
-	public static final String NAME_VALIDATION_REGEXP = "[a-zA-Zˆ¸Ûı˙È·˚Ì÷‹”’⁄€¡…Õ]{1,20}"; //1-20ig helyes
+	public static final String NAME_VALIDATION_REGEXP = "[a-zA-Z√∂√º√≥√µ√∫√©√°√ª√≠√ñ√ú√ì√ï√ö√õ√Å√â√ç]{1,20}"; //1-20ig helyes
 	public static final String USER_NAME_VALIDATION_REGEXP = "[a-zA-Z\\d]{1,20}";
 	/**
 	 * Minimum 8 characters at least 1 Uppercase Alphabet, 1 Lowercase Alphabet, 1 Number and 1 Special Character:
 	 */
 	public static final String PASSWORD_REGEXP = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,}";
+	public static final String EMAIL_VALIDATION_REGEXP = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+			+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 }
