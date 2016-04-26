3f8230ec6eac91d7f1a1a22bd5796c6f5913b1ef
Beni312 <bence312@gmail.com>
2016-04-26 23:26:02 +0200

    Create UserRegistrationRequestTest.java

---
+++ manuscript-core/manuscript-core-controller/src/test/java/iit/uni/miskolc/controller/userRegistrationRequest/UserRegistrationRequestTest.java
@@ -0,0 +1,92 @@
+package iit.uni.miskolc.controller.userRegistrationRequest;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import iit.uni.miskolc.controller.request.UserRegistrationRequest;
+import iit.uni.miskolc.exception.handling.BusinessMessageAwareException;
+import iit.uni.miskolc.model.address.Address;
+
+public class UserRegistrationRequestTest {
+
+	UserRegistrationRequest request;
+	
+	@Before
+	public void setUp() throws Exception {
+		request = new UserRegistrationRequest();
+		request.setFirstName("Kiss");
+		request.setLastName("Pista");
+		request.setUserName("ASD123");
+		request.setPassword("ASDeuhwefjd23!");
+		SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = fmt.parse("2001-05-06");
+		request.setBirthDate(date);
+		request.setPersonalDescription("Description");
+		request.setEmail("email@email.hu");
+		request.setPhoneNumber("+36304545678");
+		Address address = new Address();
+		request.setAddress(address);
+	}
+	
+	@Test
+	public void createUserWithValidDatas() throws BusinessMessageAwareException{
+		request.createUser();
+	}
+	
+	@Test (expected = BusinessMessageAwareException.class)
+	public void createUserWithWrongFirstName() throws BusinessMessageAwareException{
+		request.setFirstName("!");
+		request.createUser();
+	}
+	
+	@Test (expected = BusinessMessageAwareException.class)
+	public void createUserWithWrongLastName() throws BusinessMessageAwareException{
+		request.setLastName("!");
+		request.createUser();
+	}
+	
+	@Test (expected = BusinessMessageAwareException.class)
+	public void createUserWithWrongUserName() throws BusinessMessageAwareException{
+		request.setUserName("Ő");
+		request.createUser();
+	}
+	
+	@Test (expected = BusinessMessageAwareException.class)
+	public void createUserWithWrongPassword() throws BusinessMessageAwareException{
+		request.setPassword("wrongpassword");
+		request.createUser();
+	}
+
+	@Test (expected = BusinessMessageAwareException.class)
+	public void createUserWithWrongDate() throws BusinessMessageAwareException{
+		SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = null;
+		try {
+			date = fmt.parse("2019-05-06");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		request.setBirthDate(date);
+		request.createUser();
+	}
+	
+	@Test (expected = BusinessMessageAwareException.class)
+	public void createUserWithTooLongDescription() throws BusinessMessageAwareException{
+		String longDescription = "";
+		for (int i = 0; i < 251; i++)
+			longDescription += "c";
+		request.setPersonalDescription(longDescription);
+		request.createUser();
+	}
+	
+	@Test (expected = BusinessMessageAwareException.class)
+	public void createUserWithWrongEmailAddress() throws BusinessMessageAwareException{
+		request.setEmail("email@gmail.hu.");
+		request.createUser();
+	}
+	
+}
