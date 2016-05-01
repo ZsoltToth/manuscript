8bebfdc00782165da04276b715a7b67d9e991393
Beni312 <bence312@gmail.com>
2016-05-01 20:16:49 +0200

    Update UserRegistrationRequestTest.java

--- manuscript-core/manuscript-core-controller/src/test/java/iit/uni/miskolc/controller/userRegistrationRequest/UserRegistrationRequestTest.java
+++ manuscript-core/manuscript-core-controller/src/test/java/iit/uni/miskolc/controller/userRegistrationRequest/UserRegistrationRequestTest.java
@@ -1,11 +1,14 @@
 package iit.uni.miskolc.controller.userRegistrationRequest;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import iit.uni.miskolc.controller.request.UserRegistrationRequest;
+import iit.uni.miskolc.exception.handling.UserValidationException;
 import iit.uni.miskolc.model.address.Address;
 
 public class UserRegistrationRequestTest {
@@ -29,61 +32,61 @@ public class UserRegistrationRequestTest {
 		request.setAddress(address);
 	}
 	
-//	@Test
-//	public void createUserWithValidDatas() throws BusinessMessageAwareException{
-//		request.createUser();
-//	}
-//	
-//	@Test (expected = BusinessMessageAwareException.class)
-//	public void createUserWithWrongFirstName() throws BusinessMessageAwareException{
-//		request.setFirstName("!");
-//		request.createUser();
-//	}
-//	
-//	@Test (expected = BusinessMessageAwareException.class)
-//	public void createUserWithWrongLastName() throws BusinessMessageAwareException{
-//		request.setLastName("!");
-//		request.createUser();
-//	}
-//	
-//	@Test (expected = BusinessMessageAwareException.class)
-//	public void createUserWithWrongUserName() throws BusinessMessageAwareException{
-//		request.setUserName("Ĺ�");
-//		request.createUser();
-//	}
-//	
-//	@Test (expected = BusinessMessageAwareException.class)
-//	public void createUserWithWrongPassword() throws BusinessMessageAwareException{
-//		request.setPassword("wrongpassword");
-//		request.createUser();
-//	}
-//
-//	@Test (expected = BusinessMessageAwareException.class)
-//	public void createUserWithWrongDate() throws BusinessMessageAwareException{
-//		SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
-//		Date date = null;
-//		try {
-//			date = fmt.parse("2019-05-06");
-//		} catch (ParseException e) {
-//			e.printStackTrace();
-//		}
-//		request.setBirthDate(date);
-//		request.createUser();
-//	}
-//	
-//	@Test (expected = BusinessMessageAwareException.class)
-//	public void createUserWithTooLongDescription() throws BusinessMessageAwareException{
-//		String longDescription = "";
-//		for (int i = 0; i < 251; i++)
-//			longDescription += "c";
-//		request.setPersonalDescription(longDescription);
-//		request.createUser();
-//	}
-//	
-//	@Test (expected = BusinessMessageAwareException.class)
-//	public void createUserWithWrongEmailAddress() throws BusinessMessageAwareException{
-//		request.setEmail("email@gmail.hu.");
-//		request.createUser();
-//	}
+	@Test
+	public void createUserWithValidDatas() throws UserValidationException{
+		request.createUser();
+	}
+	
+	@Test (expected = UserValidationException.class)
+	public void createUserWithWrongFirstName() throws UserValidationException{
+		request.setFirstName("!");
+		request.createUser();
+	}
+	
+	@Test (expected = UserValidationException.class)
+	public void createUserWithWrongLastName() throws UserValidationException{
+		request.setLastName("!");
+		request.createUser();
+	}
+	
+	@Test (expected = UserValidationException.class)
+	public void createUserWithWrongUserName() throws UserValidationException{
+		request.setUserName("Äąďż˝");
+		request.createUser();
+	}
+	
+	@Test (expected = UserValidationException.class)
+	public void createUserWithWrongPassword() throws UserValidationException{
+		request.setPassword("wrongpassword");
+		request.createUser();
+	}
+
+	@Test (expected = UserValidationException.class)
+	public void createUserWithWrongDate() throws UserValidationException{
+		SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = null;
+		try {
+			date = fmt.parse("2019-05-06");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		request.setBirthDate(date);
+		request.createUser();
+	}
+	
+	@Test (expected = UserValidationException.class)
+	public void createUserWithTooLongDescription() throws UserValidationException{
+		String longDescription = "";
+		for (int i = 0; i < 251; i++)
+			longDescription += "c";
+		request.setPersonalDescription(longDescription);
+		request.createUser();
+	}
+	
+	@Test (expected = UserValidationException.class)
+	public void createUserWithWrongEmailAddress() throws UserValidationException{
+		request.setEmail("email@gmail.hu.");
+		request.createUser();
+	}
 	
 }
