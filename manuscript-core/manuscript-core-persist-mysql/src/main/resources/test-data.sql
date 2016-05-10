fdc938a61a37cb52e2c3055d60fcbb4cb25ea798
Beni312 <bence312@gmail.com>
2016-05-10 22:58:21 +0200

    Create test-data.sql

---
+++ manuscript-core/manuscript-core-persist-mysql/src/main/resources/test-data.sql
@@ -0,0 +1,11 @@
+insert into users (username, password) values("Pista12","PassworD12!");
+insert into users (username, password) values("Patrik12","PassworD12!");
+insert into users (username, password) values("Dani12","PassworD12!");
+
+insert into user_roles (username, role) values("Pista12","ROLE_USER");
+insert into user_roles (username, role) values("Patrik12","ROLE_USER");
+insert into user_roles (username, role) values("Dani12","ROLE_ADMIN");
+
+insert into user_datas (username, firstname, lastname, birthdate, personal_description, registration_date, email, phone_number, street, city, postcode, country) values("Pista12","Kiss","Pista","1995-05-06","description","2016-05-10","asd@gmail.com","+36304567891","street","city",3515,"country");
+insert into user_datas (username, firstname, lastname, birthdate, personal_description, registration_date, email, phone_number, street, city, postcode, country) values("Patrik12","Nagy","Patrik","2005-05-06","description","2016-05-10","qwe@gmail.com","+36304567491","street","city",3515,"country");
+insert into user_datas (username, firstname, lastname, birthdate, personal_description, registration_date, email, phone_number, street, city, postcode, country) values("Dani12","Mezei","Dani","1994-05-06","description","2016-05-10","yxc@gmail.com","+36304567491","street","city",3515,"country");
