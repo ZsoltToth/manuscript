78af5f478f59a86df5ac106fc6894016f21a9191
Balázs Kovács <kovats.balazs25@gmail.com>
2017-04-11 20:40:24 +0200

    manuscript test module for test
    this component will contains the basic stuffs that related to tests

---
+++ framework/manuscript-test/src/main/java/manuscript/test/dao/context/AbstractDaoTestContext.java
@@ -0,0 +1,39 @@
+package manuscript.test.dao.context;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+/**
+ * 
+ * @author Balazs Kovacs
+ *
+ */
+public abstract class AbstractDaoTestContext {
+	
+	public static final String DATABASE_DRIVER_CLASS = "com.mysql.jdbc.Driver";
+	public static final String DATABASE_URL = "jdbc:mysql://localhost:3306/manuscript";
+	public static final String DATABASE_USER_NAME = "root";
+	public static final String DATABASE_PASSWORD = "";
+	
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(DATABASE_DRIVER_CLASS);
+		dataSource.setUrl(DATABASE_URL);
+		dataSource.setUsername(DATABASE_USER_NAME);
+		dataSource.setPassword(DATABASE_PASSWORD);
+		return dataSource;
+	}
+	
+	@Bean
+	public SqlSessionFactory getSqlSessionFactory() throws Exception {
+		SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+		sqlSessionFactory.setDataSource(dataSource());
+		return sqlSessionFactory.getObject();
+	}
+}
